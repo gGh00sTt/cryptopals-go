7665d1bd2223781141031af73601adebac4e2019
gGh00sTt <coolmagnas@gmail.com>
2023-08-20 20:00:04 +0545

    :tada: Add set1-challenge1 solution

---
+++ 1-1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+)
+
+func main() {
+
+	input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	convbyte, err := hex.DecodeString(input)
+	if err != nil {
+		panic(err)
+	}
+	convbase64 := base64.StdEncoding.EncodeToString(convbyte)
+	println(convbase64)
+
+}
